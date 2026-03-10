4eff0358725a85b5a28a632228f00a4d5c7e0b86
ElDeutch <kevin.staalberg@gmail.com>
2026-03-10 14:14:48 +0100

    imgTracking fix
    la till ett script å nu funkar imgTracking kung

---
+++ Assets/Scripts/ImageTracking/ImageTrackingSetup.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTrackingSetup : MonoBehaviour
+{
+    [SerializeField] ARTrackedImageManager trackedImageManager;
+
+    void Awake()
+    {
+        trackedImageManager.requestedMaxNumberOfMovingImages = 1;
+    }
+
+    void OnEnable()
+    {
+        trackedImageManager.trackablesChanged.AddListener(OnTrackedImagesChanged);
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackablesChanged.RemoveListener(OnTrackedImagesChanged);
+    }
+
+    void OnTrackedImagesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> args)
+    {
+        foreach (var image in args.updated)
+        {
+            UnityEngine.Debug.Log($"Tracking state: {image.trackingState}");
+        }
+    }
+}
