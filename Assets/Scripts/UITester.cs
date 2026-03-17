be29f05fed3af5a5e9f8c7b1e528beabd35b33ef
ElDeutch <kevin.staalberg@gmail.com>
2026-03-17 14:03:22 +0100

    UI-Tester
    Tester för att se så UI funkar

---
+++ Assets/Scripts/UITester.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class UITester : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            UIManager.Instance?.ShowStartScreen();
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            UIManager.Instance?.ShowGameUI();
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            UIManager.Instance?.ShowGameOver();
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            UIManager.Instance?.UpdateScore(999);
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            UIManager.Instance?.UpdateTimer(8f); // testar röd timer
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            UIManager.Instance?.UpdateRecipe("Köttbullar");
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            UIManager.Instance?.ShowTemporaryMessage("FEL! -1 liv", 2f, "Sallad");
+    }
+}
